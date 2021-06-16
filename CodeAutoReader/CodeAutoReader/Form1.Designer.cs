8b30536438282033b2ac0c236b80a45a28e093f5
PavelGem <pashentsev.1@gmail.com>
2021-06-16 08:08:29 +0300

    add One Level Scan

--- CodeAutoReader/CodeAutoReader/Form1.Designer.cs
+++ CodeAutoReader/CodeAutoReader/Form1.Designer.cs
@@ -33,11 +33,17 @@ namespace CodeAutoReader
             this.result = new System.Windows.Forms.TextBox();
             this.extensions = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // button
             // 
-            this.button.Location = new System.Drawing.Point(12, 12);
+            this.button.Location = new System.Drawing.Point(334, 30);
             this.button.Name = "button";
             this.button.Size = new System.Drawing.Size(102, 23);
             this.button.TabIndex = 0;
@@ -47,7 +53,7 @@ namespace CodeAutoReader
             // 
             // result
             // 
-            this.result.Location = new System.Drawing.Point(120, 12);
+            this.result.Location = new System.Drawing.Point(120, 87);
             this.result.Multiline = true;
             this.result.Name = "result";
             this.result.Size = new System.Drawing.Size(316, 201);
@@ -55,14 +61,15 @@ namespace CodeAutoReader
             // 
             // extensions
             // 
-            this.extensions.Location = new System.Drawing.Point(13, 42);
+            this.extensions.Location = new System.Drawing.Point(12, 84);
             this.extensions.Name = "extensions";
             this.extensions.Size = new System.Drawing.Size(100, 20);
             this.extensions.TabIndex = 2;
+            this.extensions.TextChanged += new System.EventHandler(this.extensions_TextChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(13, 190);
+            this.button1.Location = new System.Drawing.Point(12, 265);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(99, 23);
             this.button1.TabIndex = 3;
@@ -70,11 +77,72 @@ namespace CodeAutoReader
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(316, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Directory path: (C:\\Project\\Scripts)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Extensions: (.cs,.py)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(117, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Result:";
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(12, 134);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(98, 23);
+            this.startButton.TabIndex = 8;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 111);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(100, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "MultiLevel scan";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(448, 225);
+            this.ClientSize = new System.Drawing.Size(448, 300);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.extensions);
             this.Controls.Add(this.result);
@@ -93,6 +161,12 @@ namespace CodeAutoReader
         private System.Windows.Forms.TextBox result;
         private System.Windows.Forms.TextBox extensions;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- CodeAutoReader/CodeAutoReader/Form1.cs
+++ CodeAutoReader/CodeAutoReader/Form1.cs
@@ -34,17 +34,18 @@ namespace CodeAutoReader
             if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
             {
                 folderName = folderBrowserDialog.SelectedPath;
-                extensionsArray = extensions.Text.Split(',');
-                text = GetCodeFromDirectory(folderName);
-                result.Text = text;
+                textBox1.Text = folderName;
             }
         }
         string GetCodeFromDirectory(string direcotyPath) 
         {
             string result = "";
-            foreach (var item in Directory.GetDirectories(direcotyPath))
+            if (checkBox1.Checked)
             {
-                result += GetCodeFromDirectory(item);
+                foreach (var item in Directory.GetDirectories(direcotyPath))
+                {
+                    result += GetCodeFromDirectory(item);
+                }
             }
             foreach (var item in Directory.GetFiles(direcotyPath))
             {
@@ -74,6 +75,25 @@ namespace CodeAutoReader
         private void button1_Click_1(object sender, EventArgs e)
         {
             Clipboard.SetText(text);
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(folderName))
+            {
+                text = GetCodeFromDirectory(folderName);
+                result.Text = text;
+            }
+        }
+
+        private void extensions_TextChanged(object sender, EventArgs e)
+        {
+            extensionsArray = extensions.Text.Split(',');
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            folderName = textBox1.Text;
         }
     }
 }
